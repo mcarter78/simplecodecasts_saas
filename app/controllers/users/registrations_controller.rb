7e91d0c72159a1b0d0bbf099ed5eb72c61f90ef3
Matt Carter <sjsharksftw@gmail.com>
2015-07-30 02:37:51 +0000

    Fixed typon in registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Users::RegstrationsController < Devise::RegistrationsController
+class Users::RegistrationsController < Devise::RegistrationsController
   
   def create
     super do |resource|
